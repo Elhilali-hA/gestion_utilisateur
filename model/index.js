3558f3772af08112ad6cf1550de8bda3ab235d20
Elhilali-hA <hamzaelhilali8@gmail.com>
2022-01-19 11:10:47 +0100

    init

--- model/index.js
+++
@@ -1,6 +0,0 @@
-const User = require('./user')
-const depart = require('./departement')
-
-
-User.belongsTo(depart, {foreignKey: 'depart_id'})
-depart.hasmany(User)
--- model/user.js
+++ model/user.js
@@ -21,5 +21,5 @@ module.exports = (sequelize, Sequelize) => {
 	  }
 	});
 	
-
+ return User
 }
